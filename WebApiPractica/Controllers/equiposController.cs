a72dc0baf9fc089a921a38095b3bd6c9d8ad98b9
Gustavo Cuéllar <Gustavo Cullar@TAVOJAJA>
2023-02-26 18:09:09 -0600

    Agregar archivos de proyecto.

---
+++ WebApiPractica/Controllers/equiposController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiPractica.ModelsXD;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiPractica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly equiposContext _equiposContexto;
+
+    }
+
+    public equiposController(equiposContext equiposContexto) 
+    {
+        _equiposContexto = equiposContexto;
+    }
+
+    [HttpGet]
+    [Route("GetAll")]
+
+    public IActionsResult Get() 
+    {
+        List<equipos> listadoEquipo = (from e in _equiposContexto.equipos select e).ToList();
+
+        if (ListadoEquipo.Count() =0) 
+        {
+            return NotFound();
+        }
+
+        return Ok(listadoEquipo);
+    }
+
+    public IActionsResult Get() 
+    {
+        List<equipos> listadoEquipo = (from e in _equiposContexto.equipos select e).ToList();
+
+        if (listadoEquipo.Count() == 0) 
+        {
+            return NotFound();
+        }
+
+        return Ok(listadoEquipo);
+    }
+
+    [HttpGet]
+    [Route("GetById/{id}")]
+    public IActionsResult Get(int id) 
+    {
+        equipos? equipo = (from e in _equiposContexto.equipos
+                           where e.id_equipos == id
+                           select e).FirstOrDefault();
+
+        if (equipo == null) 
+        {
+            return NotFound();
+
+        }
+        return Ok(equipo);
+    }
+
+    [HttpPost]
+    [Route("Add")]
+
+    public IActionResult GuardarEquipo([FromBody] equipos equipo) 
+    {
+        try 
+        {
+            _equiposContexto.equipos.Add(equipo);
+            _equiposContexto.SaveChanges();
+            return Ok(equipo);
+        }
+
+        catch (Exception ex)
+        {
+            retunr BadRequest(Exception.Messages);
+        }
+    }
+
+    [HttpPut]
+    [Route("actualizar/{id}")]
+    
+    public IActionResult ActualizarEquipo (int id, [FromBody] equipos equipoModificar) 
+    {
+        equipos? equipoActual = (from e in _equiposContexto.equipos
+                                 where e.id_equipos == id
+                                 select e).FirstOrDefault();
+        if (equipoActual == null) 
+        {return NotFound();}.
+
+        equipoActual.nombre = equipoModificar.nombre;
+        equipoActual.descripcion = equipoModificar.descripcion;
+        equipoActual.marca_id = equipoModificar.marca_id;
+        equipoModificar.tipo_equipo_id = equipoModificar.tipo_equipo_id;
+        equipoActual.anio_compra = equipoModificar.anio_compra;
+        equipoActual.costo = equipoModificar.costo;
+
+        _equiposContexto.Entry(equipoActual).State = EntityState.Modified;
+        _equiposContexto.SaveChanges();
+
+        return Ok(equipoModificar);
+    }
+
+    [HttpDelete]
+    [Route("eliminar/{id}")]
+
+    public IActionsResult EliminarEquipo (int id) 
+    {
+        equipos? equipo = (from e in _equiposContexto.equipos
+                           where e.id_equipos == id
+                           select e).FirstOrDefault();
+
+        if (equipo == null)
+            return NotFound();
+
+        _equiposContexto.equipos.Attach(equipo);
+        _equiposContexto.equipos.Remove(equipo);
+        _equiposContexto.SaveChanges();
+
+        return Ok(equipo);
+    }
+
+}
---
+++ WebApiPractica/ModelsXD/EquiposContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace WebApiPractica.ModelsXD
+{
+    public class equiposContext : DbContext 
+    {
+        public equiposContext (DbContextOptions<equiposContext>options) : base(options) 
+        {
+        }
+
+        public DbSet<equipos> equipos { get; set; } 
+    }
+}
